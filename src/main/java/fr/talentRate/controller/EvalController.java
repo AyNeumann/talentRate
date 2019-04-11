e67396ce22d73a369a36cf266f7cc7b19eaa474d
Aymeric <ay.neumann@laposte.net>
2019-04-11 12:03:58 +0200

    #11 CORS setup up by configuration file

--- src/main/java/fr/talentRate/controller/EvalController.java
+++ src/main/java/fr/talentRate/controller/EvalController.java
@@ -12,7 +12,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +32,6 @@ import fr.talentRate.service.EvalService;
  * @author Aymeric
  *
  */
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/eval")
 public class EvalController {
